61a458571facd52656072eb3b1c98b085cafe5a3
kushagrasarathe <76868364+kushagrasarathe@users.noreply.github.com>
2022-11-09 15:11:18 +0530

    dashboard layout  added

--- app/pages/_app.js
+++ app/pages/_app.js
@@ -1,7 +1,37 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+
+import "@rainbow-me/rainbowkit/styles.css";
+
+import { getDefaultWallets, RainbowKitProvider } from "@rainbow-me/rainbowkit";
+import { chain, configureChains, createClient, WagmiConfig } from "wagmi";
+import { alchemyProvider } from "wagmi/providers/alchemy";
+import { publicProvider } from "wagmi/providers/public";
+import Layout from "../components/Layout";
+
+const { chains, provider } = configureChains(
+  [chain.polygonMumbai],
+  [alchemyProvider({ apiKey: process.env.ALCHEMY_ID }), publicProvider()]
+);
+const { connectors } = getDefaultWallets({
+  appName: "My RainbowKit App",
+  chains,
+});
+const wagmiClient = createClient({
+  autoConnect: true,
+  connectors,
+  provider,
+});
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <WagmiConfig client={wagmiClient}>
+      <RainbowKitProvider chains={chains}>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </RainbowKitProvider>
+    </WagmiConfig>
+  );
 }
 
-export default MyApp
+export default MyApp;
---
+++ app/pages/dashboard/index.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import DashboardLayout from '../../components/DashboardLayout'
+
+export default function DashboardHome() {
+  return (
+    <div>
+        <DashboardLayout>
+
+            <h1>Kushagra</h1>
+            <h1>Kushagra</h1>
+            <h1>Kushagra</h1>
+            <h1>Kushagra</h1>
+            <h1>Kushagra</h1>
+            <h1>Kushagra</h1>
+            <h1>Kushagra</h1>
+            <h1>Kushagra</h1>
+            <h1>Kushagra</h1>
+            <h1>Kushagra</h1>
+            <h1>Kushagra</h1>
+            <h1>Kushagra</h1>
+            <h1>Kushagra</h1>
+            <h1>Kushagra</h1>
+            <h1>Kushagra</h1>
+            <h1>Kushagra</h1>
+            <h1>Kushagra</h1>
+        </DashboardLayout>
+        
+    </div>
+  )
+}
