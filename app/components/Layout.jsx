0a285f6738d9102f7e760ac3ad0272d05358fae1
kushagrasarathe <76868364+kushagrasarathe@users.noreply.github.com>
2022-11-09 14:28:41 +0530

    home page navbar added

--- app/components/Layout.jsx
+++ app/components/Layout.jsx
@@ -1,7 +1,53 @@
-import React from 'react'
+import styles from "../styles/Layout.module.css";
+import logo from "../assets/navLogo.png";
+import Image from "next/image";
+import Link from "next/link";
 
-export default function Layout() {
+export default function Layout({ children }) {
   return (
-    <div>Layout</div>
-  )
+    <>
+      <nav className={styles.navbar}>
+        <div className={styles.logo}>
+          <Link href={"/"}>
+            <Image src={logo} />
+          </Link>
+        </div>
+
+        <ul>
+          <li className={styles.navLink}>
+            <Link href={"/dashboard"}>Dashboard</Link>
+          </li>
+        </ul>
+      </nav>
+
+      <div className="min-h-screen">
+        {children}
+      </div>
+
+      <footer className={styles.footer}>
+        <h4>
+          Built by{" "}
+          <u>
+            <a
+              href="https://twitter.com/kushagrasarathe"
+              target="_blankspace"
+              rel="noreferrer"
+            >
+              Kushagra Sarathe
+            </a>
+          </u>
+          <span> &#38; </span>
+          <u>
+            <a
+              href="https://twitter.com/0xdhruva"
+              target="_blankspace"
+              rel="noreferrer"
+            >
+              Dhruv Agarwal
+            </a>
+          </u>
+        </h4>
+      </footer>
+    </>
+  );
 }
