cda55e030cf6d12b209417ba7c3b469a1a6f4c33
kushagrasarathe <76868364+kushagrasarathe@users.noreply.github.com>
2022-11-18 17:00:59 +0530

    personl page mock

--- app/pages/pay/[address]/index.js
+++ app/pages/pay/[address]/index.js
@@ -1,10 +1,18 @@
 /// for personalized page
-import React from 'react'
+import React from "react";
 
 export default function Home() {
   return (
     <div>
-        Personalized payment page
+      <div>
+        <h1>Kushagra Sarathe</h1>
+        <span>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium
+          optio, eum velit doloremque officia nostrum dignissimos officiis?
+          Provident harum soluta expedita, nemo repudiandae ab vel cum culpa!
+          Quos, sapiente esse!
+        </span>
+      </div>
     </div>
-  )
+  );
 }
