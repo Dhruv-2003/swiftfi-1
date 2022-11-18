7fdd270806866022a9b4957e881aeaf9b03d34c3
kushagrasarathe <76868364+kushagrasarathe@users.noreply.github.com>
2022-11-18 15:49:46 +0530

    paylink page design chage

--- app/pages/pay/[address]/[id].js
+++ app/pages/pay/[address]/[id].js
@@ -5,11 +5,13 @@ import styles from "../../../styles/Home.module.css";
 /// for payment Request
 export default function Id() {
   return (
-    <div className={`${styles.bg} flex items-start justify-center flex-wrap`}>
+    <div
+      className={`${styles.bg} px-6 min-h-screen bg-[#0000008b] flex items-center justify-center flex-wrap`}
+    >
       <div
-        className={`${styles.profileCard}  flex flex-col items-center justify-start w-full h-[300px]`}
+        className={`${styles.paylink} flex flex-col items-center justify-start w-full `}
       >
-        <h1 className="lg:mt-[40px] mt-[0px] pb-2 text-lg md:text-2xl font-semibold text-center">
+        <h1 className="pt-6 pb-2 text-lg md:text-2xl font-semibold text-center">
           gm{" "}
           <span className="text-xl md:text-2xl font-semibold">
             Kushagra Sarathe
@@ -31,22 +33,20 @@ export default function Id() {
           has requested you <span className="text-lg font-semibold">$5000</span>
         </h1>
         <div>
-        <SwiftPay/>
-      </div>
+          <SwiftPay />
+        </div>
+        <div className="max-w-[700px] md:mt-[50px]">
+          <h1 className="text-3xl  font-semibold pb-8 text-center">
+            <span className="underline">Here&#39;s a note for you</span>🫡
+          </h1>
+          <p className="text-center px-4 pb-8">
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Id
+            aspernatur, pariatur consequuntur optio error odit quaerat officia
+            explicabo obcaecati. Quis, tempora quia. Rerum sed, doloremque ipsa
+          </p>
+        </div>
       </div>
 
-      <div className="max-w-[700px] mt-[230px] md:mt-[50px]">
-        <h1 className="text-3xl  font-semibold pb-8 text-center">
-          <span className="underline">Here&#39;s a note for you</span>🫡
-        </h1>
-        <p className="text-center px-4 pb-5">
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Id
-          aspernatur, pariatur consequuntur optio error odit quaerat officia
-          explicabo obcaecati. Quis, tempora quia. Rerum sed, doloremque ipsa
-        </p>
-      </div>
-
-      
       {/* <div className={styles.align}>
         <Phone />
       </div> */}
